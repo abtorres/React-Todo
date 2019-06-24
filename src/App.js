16c7d8e5549a4094ebf84cd6d4d7947d20664491
abtorres <abtorres079@gmail.com>
2019-06-24 00:12:27 -0600

    items can be added, local storage in use

--- src/App.js
+++ src/App.js
@@ -1,13 +1,51 @@
 import React from 'react';
+import Todo from './components/TodoComponents/Todo';
+import './app.css';
 
 class App extends React.Component {
   // you will need a place to store your state in this component.
   // design `App` to be the parent component of your application.
   // this component is going to take care of state, and any change handlers you need to work with your state
+  constructor() {
+    super();
+    this.state = {
+      todos: [],
+      value: ''
+    };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({value: event.target.value});
+  }
+
+  handleSubmit(event) {
+    this.setState({todos: this.state.todos.concat(this.state.value)});
+    event.preventDefault();
+    this.setState({value: ''});
+  }
+
+  componentDidUpdate(){
+    localStorage.setItem('todos', JSON.stringify(this.state.todos));
+  }
+
+
+  componentDidMount(){
+    let ttodos = JSON.parse(localStorage.getItem('todos'));
+    if(ttodos){
+      this.setState({todos: ttodos});
+    }else {
+      this.setState({todos: []})
+    }
+    
+  }
+
   render() {
     return (
-      <div>
+      <div className='app'>
         <h2>Welcome to your Todo App!</h2>
+        <Todo todos={this.state.todos} value={this.state.value} onChange={this.handleChange} onSubmit={this.handleSubmit}/>
       </div>
     );
   }
