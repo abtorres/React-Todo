3615544d0720a05b5adabf6ee022a08cf159d39e
abtorres <abtorres079@gmail.com>
2019-09-20 05:26:49 -0600

    fixed issue

--- src/components/TodoComponents/Todo.js
+++ src/components/TodoComponents/Todo.js
@@ -1,19 +1,3 @@
-import React, { Component } from 'react'
-import TodoForm from './TodoForm'
-import './Todo.css';
-
-export class Todo extends Component {
-    render(props) {
-        return (
-            <div className='todos'>
-                <div className='todo'>{this.props.todos.map((todo, i) => <h3 key={i}>{todo}</h3>)}</div>
-                <TodoForm value={this.props.value} onChange={this.props.onChange} onSubmit={this.props.onSubmit}/>
-            </div>
-        )
-    }
-}
-
-export default Todo
 import React from 'react';
 import './Todo.css';
 import styled from 'styled-components';
